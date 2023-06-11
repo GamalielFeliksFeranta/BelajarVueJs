f06837a690adcb9492143186dc58a45009925244
GamalielFeliksFeranta <gferanta@student.ciputra.ac.id>
2023-06-11 22:54:13 +0700

    menambahkan main.js dan style.css dan mencoba memanggil beberapa atribute dari main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+var app= new Vue({
+    el:'#app',
+    data:{
+        product:'Socks',
+        image: './images/kaoskaki1.jpg',
+        link :'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg', 
+        inventory:0,
+        onSale:true,
+        details:["80% cotton", "20% polyester","Gender-neutral"],
+        variants:[{
+            variandId:2234,
+            variantColor:"green"},
+            {
+            variandId:2235,
+            variantColor:"blue"},
+            
+        ],
+        sizes:["L","XL","S"]
+    }
+})
